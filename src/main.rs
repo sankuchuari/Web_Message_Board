3544e5ff7fde6e2bdd82b5538e07ea9d2ccf5e0c
sankuchuari <159986836+sankuchuari@users.noreply.github.com>
2026-05-07 23:08:04 +0800

    新增I18n编辑字段

--- src/main.rs
+++ src/main.rs
@@ -309,7 +309,8 @@ async fn index(db: web::Data<SqlitePool>, session: Session) -> impl Responder {
                             loginUser: "用户名", loginPass: "密码",
                             signin: "登录", signup: "注册", logout: "🚪 退出",
                             textPh: "说点什么...", submit: "发布留言", list: "历史留言：",
-                            del: "删除", empty: "暂无留言，快来抢沙发！", view: "📄 查看文件: ",
+                            del: "删除", edit: "编辑", save: "保存", cancel: "取消",
+                            empty: "暂无留言，快来抢沙发！", view: "📄 查看文件: ",
                             tipSuccess: "✅ 登录成功！", tipNoUser: "❌ 用户名或密码错误", tipWrong: "❌ 用户名或密码错误", tipReg: "📝 注册成功！现在请登录。", tipConflict: "⚠️ 该用户名已被注册"
                         }
                     };
