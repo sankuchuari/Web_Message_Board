4b6d6d8d448643a5156b4cd78cf8fdff18b71f71
sankuchuari <159986836+sankuchuari@users.noreply.github.com>
2026-05-08 23:31:48 +0800

    开启https

--- src/main.rs
+++ src/main.rs
@@ -713,7 +713,7 @@ async fn main() -> io::Result<()> {
             // Session 配置
             .wrap(
                 SessionMiddleware::builder(CookieSessionStore::default(), key.clone())
-                    .cookie_secure(false)// 仅通过 HTTPS 传输
+                    .cookie_secure(true)// 仅通过 HTTPS 传输
                     .cookie_same_site(SameSite::Lax)// 缓解 CSRF 攻击
                     .cookie_http_only(true) // 禁止客户端 JS 读取 Session Cookie，防御 XSS 劫持
                     .build()
