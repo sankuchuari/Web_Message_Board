7e20b1b3de9fc1773636809f7f257d2613470b96
sankuchuari <159986836+sankuchuari@users.noreply.github.com>
2026-05-17 10:17:45 +0800

    优化注释

--- src/main.rs
+++ src/main.rs
@@ -859,6 +859,7 @@ async fn post_message(mut payload: Multipart, db: web::Data<SqlitePool>, session
     if !message.trim().is_empty() || !images.is_empty() || !others.is_empty() {
         // XSS 防御：在存储前也进行一次清洗
         let safe_html = markdown_to_html(&message);
+        //图片地址
         let img_str = if images.is_empty() { None } else { Some(images.join(",")) };
         //其他文件地址
         let other_str = if others.is_empty() { None } else { Some(others.join(",")) };
