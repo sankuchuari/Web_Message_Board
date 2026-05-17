50178cfe907565011b9ed780d6b2f96d126d7429
sankuchuari <159986836+sankuchuari@users.noreply.github.com>
2026-05-17 11:52:46 +0800

    路径优化

--- src/tls_ssl_certificate_register.rs
+++ src/tls_ssl_certificate_register.rs
@@ -100,7 +100,7 @@ pub(crate)fn run_python_setup() -> io::Result<()> {
 pub(crate)fn load_rustls_config() -> ServerConfig {
     let mut cert_file = io::BufReader::new(fs::File::open("cert.pem")
         .expect("无法找到 cert.pem，请确保 Python 脚本运行成功"));
-    let mut key_file = io::BufReader::new(fs::File::open("key.pem")
+    let mut key_file = io::BufReader::new(fs::File::open("./static/key.pem")
         .expect("无法找到 key.pem，请确保 Python 脚本运行成功"));
 
     let cert_chain = certs(&mut cert_file)
