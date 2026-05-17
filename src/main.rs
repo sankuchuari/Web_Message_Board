e3c9d25a40ac846b6c8aaa9385f28f6a2cfcf3d9
sankuchuari <159986836+sankuchuari@users.noreply.github.com>
2026-05-17 09:36:43 +0800

    优化格式

--- src/main.rs
+++ src/main.rs
@@ -829,7 +829,6 @@ async fn post_message(mut payload: Multipart, db: web::Data<SqlitePool>, session
         Some(u) => u,
         None => return HttpResponse::SeeOther().append_header(("Location", "/")).finish(),
     };
-
     let mut message = String::new();
     let mut images = Vec::new();
     let mut others = Vec::new();
