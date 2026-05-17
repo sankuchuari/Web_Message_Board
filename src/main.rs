2e5d9f5d9709365fa758d14861b87702c49c0064
sankuchuari <159986836+sankuchuari@users.noreply.github.com>
2026-05-17 09:32:04 +0800

    以\重新格式化编码

--- src/main.rs
+++ src/main.rs
@@ -485,13 +485,13 @@ async fn index(db: web::Data<SqlitePool>, session: Session) -> impl Responder {
                 script { (PreEscaped("
                     const i18n = {
                         en: {
-                            title: "MESSAGE BOARD", mode: "🌓 Mode", lang: "🌐 Lang",
-                            loginUser: "Username", loginPass: "Password",
-                            signin: "Sign In", signup: "Sign Up", logout: "🚪 Logout",
-                            textPh: "Write some...", submit: "Submit", list: "Message list：",
-                            del: "delete", edit: "edit", save: "save", cancel: "cancel",
-                            empty: "No messages yet. Be the first!", view: "📄 View File: ",
-                            tipSuccess: "✅ Login successful!", tipNoUser: "❌ Incorrect username or password.", tipWrong: "❌ Incorrect username or password.", tipReg: "📝 Registration successful! Now please Sign In.", tipConflict: "⚠️ Username already exists."
+                            title: \"MESSAGE BOARD\", mode: \"🌓 Mode\", lang: \"🌐 Lang\",
+                            loginUser: \"Username\", loginPass: \"Password\",
+                            signin: \"Sign In\", signup: \"Sign Up\", logout: \"🚪 Logout\",
+                            textPh: \"Write some...\", submit: \"Submit\", list: \"Message list：\",
+                            del: \"delete\", edit: \"edit\", save: \"save\", cancel: \"cancel\",
+                            empty: \"No messages yet. Be the first!\", view: \"📄 View File: \",
+                            tipSuccess: \"✅ Login successful!\", tipNoUser: \"❌ Incorrect username or password.\", tipWrong: \"❌ Incorrect username or password.\", tipReg: \"📝 Registration successful! Now please Sign In.\", tipConflict: \"⚠️ Username already exists.\"
                         },
                         zh: {
                             title: "留言板", mode: "🌓 模式", lang: "🌐 语言",
