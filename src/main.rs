d955c76b5b3cba9d5c72bd729e74c5ba59b7166e
sankuchuari <159986836+sankuchuari@users.noreply.github.com>
2026-05-17 10:18:16 +0800

    优化注释

--- src/main.rs
+++ src/main.rs
@@ -844,6 +844,7 @@ async fn post_message(mut payload: Multipart, db: web::Data<SqlitePool>, session
             if let Some(name) = filename {
                 if !name.is_empty() {
                     let ext = Path::new(&name).extension().and_then(|s| s.to_str()).unwrap_or("").to_lowercase();
+                    //获取文件名
                     let fname = format!("{}.{}", Uuid::new_v4(), sanitize(&ext));
                     //链接上传目录
                     let _ = fs::create_dir_all("uploads");
