e3d84d04e6bd4bab874c998f7c73615e89e9ea0a
sankuchuari <159986836+sankuchuari@users.noreply.github.com>
2026-05-30 23:04:16 +0800

    结构性优化

--- src/main.rs
+++ src/main.rs
@@ -187,10 +187,6 @@ async fn main() -> io::Result<()> {
             .bind(("[::]", port))?
     } else {
         let config = load_rustls_config();
-        // 启动前调用 Python 生成/更新证书
-        if let Err(e) = run_python_setup() {
-            eprintln!("警告：自动生成证书失败: {}。尝试使用现有证书...", e);
-        }
         server
             .bind_rustls_021(("0.0.0.0", port), config.clone())?
             .bind_rustls_021(("[::]", port), config)?
